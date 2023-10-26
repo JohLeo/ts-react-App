c4be26c17fa9a6c8f1b18c1a58911639aab056cc
Johanna Leonsson <johanna@leonsson.se>
2023-10-26 18:46:12 +0200

    testing interface

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom';
+import EventComponent from './events/EventComp';
 // import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+// import UserSearch from './state/UserSearch';
 
 const App = () => {
   return <div>
-    <UserSearch />
+    <EventComponent />
   </div>
 };
 
